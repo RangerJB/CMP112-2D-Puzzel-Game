c8e7268084a9926b6277f26e6ed35ec8c93f2999
Niallhampton <2501302@uad.ac.uk>
2025-11-07 10:24:58 +0000

    Audio changes

--- Assets/objects/playerMovement.cs
+++ Assets/objects/playerMovement.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    //variables
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
     private Rigidbody2D rb;
     private bool isGrounded;
+
+    //Audio Variables
+    private AudioSource source;
+    public AudioClip JumpSound;
 
     void Start()
     {
@@ -15,18 +20,41 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // Horizontal movement
+        // Left anad right movement
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
+       
+        
+        
+        
         // Jump
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+           
         }
+
+        void Start()
+        {
+
+            source = GetComponent<AudioSource>();
+
+        }
+
+        void Update()
+        {
+            if (moveInput.GetKeyDown(KeyCode.Space))
+            {
+                source.Play(JumpSound);
+            }
+        }
+  
+
+
     }
 
-    // Ground check (simple version)
+    // Ground collison
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.contacts[0].normal.y > 0.5f)
